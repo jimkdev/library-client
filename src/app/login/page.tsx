53550595788be3d1ae61c6d04a70eb35b82691b5
jimkdev <jimkdev150@gmail.com>
2025-07-22 20:17:39 +0300

    Add spacing and link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 export default function LoginPage() {
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
-        <label htmlFor="username">Password</label>
-        <input type="password" name="password" />
+        <input type="text" name="username"/>
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password"/>
+
         <button type="submit">Login</button>
+
+        <p>Don&#39;t have an account? Register <Link href={"/register"}>here</Link>!</p>
       </form>
     </div>
   )
