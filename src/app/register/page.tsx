7c3f3e216ac3ffcef36e09342f33b87b7f9fc1d6
jimkdev <jimkdev150@gmail.com>
2025-09-14 14:37:29 +0300

    Resolve conflicts

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,51 +1,75 @@
 "use client"
 import Link from "next/link";
+import {JSX} from "react";
 import {redirect} from "next/navigation";
+import RegisterButton from "@/components/buttons/register-button";
 
-export default function RegistrationPage() {
-  async function register(formData: FormData) {
-    const response: Response = await fetch(`${process.env.NEXT_PUBLIC_API_URL!}/users/register`, {
-      method: "POST",
-      body: JSON.stringify({
-        first_name: formData.get("first-name"),
-        last_name: formData.get("last-name"),
-        mobile: formData.get("mobile"),
-        username: formData.get("username"),
-        email: formData.get("email"),
-        password: formData.get("password"),
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      }
-    });
+async function handleSubmit(formData: FormData) {
+  try {
+    if (process.env.NEXT_PUBLIC_API_URL) {
+      const response: Response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/register`, {
+        method: "POST",
+        body: JSON.stringify({
+          username: formData.get("username"),
+          password: formData.get("password"),
+          mobile: formData.get("mobile"),
+          firstName: formData.get("first-name"),
+          lastName: formData.get("last-name"),
+          email: formData.get("email"),
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
 
-    if (response.ok) {
-      redirect("/login");
+      // redirect("/login");
     }
+  } catch (error) {
+    console.log(error);
   }
+}
 
+export default function RegistrationPage(): JSX.Element {
   return (
     <div>
-      <form action={register}>
+      <form action={handleSubmit}>
         <label htmlFor="first-name">First name</label>
-        <input type="text" name="first-name"/>
+        <input
+            type="text"
+            name="first-name"
+        />
 
         <label htmlFor="last-name">Last name</label>
-        <input type="text" name="last-name"/>
+        <input
+            type="text"
+            name="last-name"
+        />
 
         <label htmlFor="mobile">Mobile phone</label>
-        <input type="text" name="mobile"/>
+        <input
+            type="text"
+            name="mobile"
+        />
 
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
+        <input
+            type="text"
+            name="username"
+        />
 
         <label htmlFor="email">Email</label>
-        <input type="text" name="email"/>
+        <input
+            type="text"
+            name="email"
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
+        <input
+            type="password"
+            name="password"
+        />
 
-        <button type="submit">Sign up</button>
+        <RegisterButton label={"Register"} />
 
         <p>Already have an account? Login <Link href={"/login"}>here</Link>!</p>
       </form>
