40d82562040bfc60ac2184730561a3043c9ac1d4
jimkdev <jimkdev150@gmail.com>
2025-09-30 21:43:24 +0300

    Delete unused components

--- src/components/buttons/register-button.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import PrimaryButton from "@/components/buttons/primary-button";
-
-export default function RegisterButton(props: { label: string }) {
-  return <PrimaryButton label={props.label} type="submit" />;
-}
