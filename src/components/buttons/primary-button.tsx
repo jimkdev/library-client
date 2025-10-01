af45a2b263b8a2b41ceba84db4527f307fbc3167
jimkdev <jimkdev150@gmail.com>
2025-10-01 11:09:05 +0300

    Set cursor to pointer for primary button

--- src/components/buttons/primary-button.tsx
+++ src/components/buttons/primary-button.tsx
@@ -5,7 +5,7 @@ export default function PrimaryButton(props: {
 }) {
   return (
     <button
-      className="border-2 border-cyan-700 bg-cyan-700 text-white rounded-md mt-4 p-1 text-lg"
+      className="cursor-pointer border-2 border-cyan-700 bg-cyan-700 text-white rounded-md mt-4 p-1 text-lg"
       onClick={props.onClick}
       type={props.type ?? "button"}
     >
